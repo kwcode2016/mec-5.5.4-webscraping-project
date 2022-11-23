099b0966381b49d366e6df56394d5cacddd10de3
Ken Wee <kwhome2002@gmail.com>
2022-11-22 16:45:12 -0800

    adding follow next page function

--- scrapy_mini_project/scrapy_mini_project/spiders/quotes_spider.py
+++ scrapy_mini_project/scrapy_mini_project/spiders/quotes_spider.py
@@ -4,45 +4,17 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
         name = "quotes"
 
-        def start_requests(self):
-            urls = [
-                'http://quotes.toscrape.com/page/1/',
-                'http://quotes.toscrape.com/page/2/',
-                'http://quotes.toscrape.com/page/3/',
-                'http://quotes.toscrape.com/page/4/',
-                'http://quotes.toscrape.com/page/5/',
-            ]
-            for url in urls:
-                print(url)
-                yield scrapy.Request(url=url, callback=self.parse)
-
-        """
-        def parse(self, response):
-            page = response.url.split("/")[-2]
-            filename = 'quotes-%s.html' % page
-            with open(filename, 'wb') as f:
-                f.write(response.body)
-            self.log('Saved file %s' % filename)
-        """
-        
-
-
-
-class SomethingSpider(scrapy.Spider):
-        name = "something"
-
-        def start_requests(self):
-            urls = [
-                'http://quotes.toscrape.com/page/6/',
-                'http://quotes.toscrape.com/page/7/',
-            ]
-            for url in urls:
-                print(url)
-                yield scrapy.Request(url=url, callback=self.parse)
+        start_urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
 
         def parse(self, response):
-            page = response.url.split("/")[-2]
-            filename = 'quotes-%s.html' % page
-            with open(filename, 'wb') as f:
-                f.write(response.body)
-            self.log('Saved file %s' % filename)
+            for quote in response.css('div.quote'):
+                yield{
+                    'text': quote.css('span.text::text').get(),
+                    'author': quote.css('small.author::text').get(),
+                    'tags': quote.css('div.tags a.tag::text').getall(),
+                }
+
+
